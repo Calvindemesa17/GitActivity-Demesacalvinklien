272a377bcb8d9323521dfb2614da4c6ca0f3900e
Demesa Calvin Klien <calvinkliend@gmail.com>
2023-03-09 15:24:20 +0800

    My Educational Background added

--- git-python-demesa.py
+++ git-python-demesa.py
@@ -1,3 +1,7 @@
 print("Calvin Klien R. De Mesa")
 print("Sibuyao,Torrijos,Marinduque")
 print("August 17,1999")
+
+print("Bachelor of Science in Information Technology")
+print("Second Year College")
+print("Section A")
